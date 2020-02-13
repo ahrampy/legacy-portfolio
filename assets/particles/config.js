f2f28378e0e373e204e2f041a780e3e72fa96be4
your name here <ahrampy@gmail.com>
2020-02-13 15:18:00 -0800

    remove particles

--- assets/particles/config.js
+++ assets/particles/config.js
@@ -1,114 +1,114 @@
-particlesJS('particles-js',
-  
+particlesJS(
+  "particles-js",
+
   {
-    "particles": {
-      "number": {
-        "value": 160,
-        "density": {
-          "enable": true,
-          "value_area": 800
+    particles: {
+      number: {
+        value: 10,
+        density: {
+          enable: true,
+          value_area: 800
         }
       },
-      "color": {
-        "value": "#ffffff"
+      color: {
+        value: "#FFFFFF"
       },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
+      shape: {
+        type: "polygon",
+        stroke: {
+          width: 0,
+          color: "#000"
         },
-        "polygon": {
-          "nb_sides": 4
+        polygon: {
+          nb_sides: 6
         },
-        // "image": {
-        //   "src": "",
-        //   "width": 100,
-        //   "height": 100
+        // image: {
+        //   src: "img/github.svg",
+        //   width: 100,
+        //   height: 100
         // }
       },
-      "opacity": {
-        "value": 1,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 0.5,
-          "opacity_min": 0,
-          "sync": false
+      opacity: {
+        value: 0.1,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
         }
       },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 4,
-          "size_min": 0.3,
-          "sync": false
+      size: {
+        value: 150,
+        random: false,
+        anim: {
+          enable: true,
+          speed: 5,
+          size_min: 20,
+          sync: false
         }
       },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
+      line_linked: {
+        enable: false,
+        distance: 200,
+        color: "#ffffff",
+        opacity: 1,
+        width: 2
       },
-      "move": {
-        "enable": true,
-        "speed": 1,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 600
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
         }
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "bubble"
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: false,
+          mode: "grab"
         },
-        "onclick": {
-          "enable": true,
-          "mode": "repulse"
+        onclick: {
+          enable: true,
+          mode: "push"
         },
-        "resize": true
+        resize: true
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
           }
         },
-        "bubble": {
-          "distance": 250,
-          "size": 2,
-          "duration": 2,
-          "opacity": 0.09744926547616141,
-          "speed": 3
+        bubble: {
+          distance: 400,
+          size: 5,
+          duration: 2,
+          opacity: 8,
+          speed: 3
         },
-        "repulse": {
-          "distance": 194.89853095232283,
-          "duration": 0.4
+        repulse: {
+          distance: 200,
+          duration: 0.4
         },
-        "push": {
-          "particles_nb": 4
+        push: {
+          particles_nb: 4
         },
-        "remove": {
-          "particles_nb": 2
+        remove: {
+          particles_nb: 2
         }
       }
     },
-    "retina_detect": true
+    retina_detect: true
   }
-
 );
