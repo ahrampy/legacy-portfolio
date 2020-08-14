fbc6a14d34cbfc84a7ce32cc61af0cee506ead1c
adrian_rampy <ahrampy@gmail.com>
2020-08-14 10:33:29 -0700

    update icons

--- jQuery.js
+++ jQuery.js
@@ -1,10 +1,10 @@
 $(function () {
   let $centerX = $("html").width() / 2;
   let $centerY = $("html").height() / 2;
-  $("window").resize(
-    ($centerX = $("html").width() / 2),
-    ($centerY = $("html").height() / 2)
-  );
+  $(window).resize(() => {
+    $centerX = $("html").width() / 2;
+    $centerY = $("html").height() / 2;
+  });
   $("html").mousemove((e) => {
     $("#border").css(
       "transform",
