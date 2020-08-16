83356f9e02abcafdc0a0eed1743cf103d5e70665
adrian_rampy <ahrampy@gmail.com>
2020-08-16 16:28:55 -0700

    add all four sections

--- jQuery.js
+++ jQuery.js
@@ -41,20 +41,64 @@ $(function () {
   );
   $("#about-btn").click((e) => {
     e.preventDefault();
+    $("#about").css("display", "inline-block");
+    $("#about").css("opacity", "100%");
     $("#flip-card").css("transform", "rotateY(180deg)");
     $("#techs").slick("slickPause");
-    setTimeout(() => {
-      $("#prof-pic").css("opacity", "0%");
-      $("#techs").css("opacity", "0%");
-    }, 200);
   });
-  $("#back-btn").click((e) => {
+  $("#about-back").click((e) => {
     e.preventDefault();
+    $("#about").css("opacity", "0%");
+    setTimeout(() => {
+      $("#about").css("display", "none");
+    }, 300);
     $("#flip-card").css("transform", "rotateY(0deg)");
     $("#techs").slick("slickPlay");
+  });
+  $("#projects-btn").click((e) => {
+    e.preventDefault();
+    $("#projects").css("display", "inline-block");
+    $("#projects").css("opacity", "100%");
+    $("#flip-card").css("transform", "rotateX(180deg)");
+  });
+  $("#projects-back").click((e) => {
+    e.preventDefault();
+    $("#projects").css("opacity", "0%");
     setTimeout(() => {
-      $("#techs").css("opacity", "100%");
-      $("#prof-pic").css("opacity", "100%");
-    }, 200);
+      $("#projects").css("display", "none");
+    }, 300);
+    $("#flip-card").css("transform", "rotateX(0deg)");
+    $("#techs").slick("slickPlay");
+  });
+  $("#resume-btn").click((e) => {
+    e.preventDefault();
+    $("#resume").css("display", "inline-block");
+    $("#resume").css("opacity", "100%");
+    $("#flip-card").css("transform", "rotateX(-180deg)");
+  });
+  $("#resume-back").click((e) => {
+    e.preventDefault();
+    $("#resume").css("opacity", "0%");
+    setTimeout(() => {
+      $("#resume").css("display", "none");
+    }, 300);
+    $("#flip-card").css("transform", "rotateX(0deg)");
+    $("#techs").slick("slickPlay");
+  });
+  $("#contact-btn").click((e) => {
+    e.preventDefault();
+    $("#contact").css("display", "inline-block");
+    $("#contact").css("opacity", "100%");
+    $("#flip-card").css("transform", "rotateY(-180deg)");
+    $("#techs").slick("slickPause");
+  });
+  $("#contact-back").click((e) => {
+    e.preventDefault();
+    $("#contact").css("opacity", "0%");
+    setTimeout(() => {
+      $("#contact").css("display", "none");
+    }, 300);
+    $("#flip-card").css("transform", "rotateY(0deg)");
+    $("#techs").slick("slickPlay");
   });
 });
