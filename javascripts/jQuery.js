abc721833eec6fb5893f626be09b4b1ead1d13db
adrian_rampy <ahrampy@gmail.com>
2020-10-15 00:17:34 -0700

    reorder jQuery

--- javascripts/jQuery.js
+++ javascripts/jQuery.js
@@ -16,16 +16,19 @@ $(function () {
   var setDark = function () {
     colorMode = "dark";
     $("#lightswitch").attr("src", "./images/mode/moon-filled.png");
-    $("#border").css({ background: "linear-gradient(#9198e5, #f48e62)" });
+    $("#border").css({background: "linear-gradient(#9198e5, #f48e62)"});
     $(":root").css("--primary", "#333");
     $(":root").css("--secondary", "#fff");
   };
   var setLight = function () {
     colorMode = "light";
     $("#lightswitch").attr("src", "./images/mode/sun-filled.png");
-    $("#border").css({ background: "linear-gradient(#f48e62, #9198e5)" });
+    $("#border").css({background: "linear-gradient(#f48e62, #9198e5)"});
     $(":root").css("--primary", "#fff");
     $(":root").css("--secondary", "#333");
+  };
+  var fadeIn = function () {
+    $("#fade-in").animate({ height: "0%" }, { duration: 950, queue: false });
   };
   var resetBorder = function () {
     $("#border").css("transform", "none");
