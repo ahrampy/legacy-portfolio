6b6f5d62235315d215e20d9fb1da0ae564e93f6b
adrian_rampy <ahrampy@gmail.com>
2020-10-19 00:01:04 -0700

    rm extra fnc call

--- javascripts/jQuery.js
+++ javascripts/jQuery.js
@@ -61,7 +61,7 @@ $(function () {
   $(window).on("load", () => {
     findCenter();
     checkMobile();
-    checkDarkMode();
+    // checkDarkMode();
   });
   $(window).on("resize", () => {
     findCenter();
